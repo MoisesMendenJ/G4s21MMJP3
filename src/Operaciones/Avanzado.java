117d03443c156ff48ea844c3b835912c18859900
TOM <TOM@192.168.1.72>
2022-04-06 13:52:53 -0500

    Este Es Mi Tercer Codigo.

---
+++ src/Operaciones/Avanzado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+
+/**
+ *
+ * @author TOM
+ */
+public class Avanzado {
+    
+    public double potencia(int base, int elevacion){
+        return Math.pow(base, elevacion);
+    }
+
+    public Object potencia(Integer valueOf) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
